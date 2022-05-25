3271a7efe552c3d55eba504116e36d56ce3bb398
Andrey Pakhomov <pahomeathome@gmail.com>
2022-05-25 18:43:05 +0300

    task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById(`ingredients`)
+
+const makeItems = items => {
+  return items.map(item => {
+    const itemEl = document.createElement('li');
+    itemEl.classList.add(`item`);
+    itemEl.textContent = item;
+
+    return itemEl;
+  })
+}
+
+const createdItems = makeItems(ingredients);
+ingredientsList.append(...createdItems)
