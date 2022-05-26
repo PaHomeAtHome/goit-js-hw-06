e1f0434792a5b59c6331080187b4bfafbbdbfe9a
Andrey Pakhomov <pahomeathome@gmail.com>
2022-05-26 19:23:12 +0300

    task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButton = document.querySelector(`.change-color`);
+const bodyEl = document.querySelector(`body`);
+const colorEl = document.querySelector(`.color`);
+
+changeColorButton.addEventListener(`click`, onClickChangeColor)
+
+function onClickChangeColor() {
+  const newColor = getRandomHexColor();
+  
+  bodyEl.style.backgroundColor = `${newColor}`;
+  colorEl.textContent = newColor;
+}
