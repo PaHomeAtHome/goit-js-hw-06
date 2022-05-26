c5bf58c884a6232efeaa1a0dda97543c7e9c9b31
Andrey Pakhomov <pahomeathome@gmail.com>
2022-05-26 18:56:28 +0300

    task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formEl = document.querySelector(`.login-form`);
 
+formEl.addEventListener(`submit`, onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const Email = formElements.email.value;
+    const Password = formElements.password.value;
+
+    if (Email.length === 0 || Password.length === 0) {
+        alert("Всі поля повинні бути заповнені")
+        return;
+    }
+
+    const formData = {
+        Email,
+        Password,
+    }
+
+    console.log(formData);
+
+    formEl.reset()  
+}
